c06075a806dea048d30fb7b2c480b74a525329dc
erenyeager37 <fazillizsf543210@gmail.com>
2026-05-11 21:24:34 +0530

    Create loveu.php

---
+++ loveu.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * A message of regret, love, and longing.
+ */
+
+$message = <<<MSG
+betu tu kaha bhi ho mujhe bhi le jao , i rreallly miss u tumhare bina i feel lifeless .i didint ask why u were leaving but tumhare bhina mai thik nai hu khush nai hu .
+i really love u yaar i cant hide it iam sorry for hiding it . ek baar mujhse baat karo i realllly love you abh mujhse nai hoga i wont hold my feelings for u .
+i really want those days back when i say soemthing and u laugh ,ur voice still echoes in my mind , my heart yearns for u. 
+ha i thought i will forget u in this past one year but nai bhul paya theeke ye ek saal mai esa din nai hai ki tumhare bhare mai na sochu. 
+i really love you the bond i shared with u is not fake i really had  feelings for u . pls wapas ao na ek baar mujhse baat karo .my heart still yearns for ur presence to hear your  voice 
+.mujhe maaf kardo i didint take it much serious when u were leaving mujhe maaf kardo .pls tumhare bina i feel like my half is dead .
+pls ek baar mujhse baat karo pls ek baar pls in my life u were the precious person .pls ek baar mujhse baat karo i really care for u maybe iam selfish sare zindagi i want to be loved by u , sorry if its too much iam asking for
+.ek baar pls talk to me
+MSG;
+
+echo $message;
+
+?>
